6795bb1c3efcb7f61842e0ec1e575cd046b4c228
Michelle Ross <miross629@gmail.com>
2023-08-27 15:03:55 -0700

    Set document title for /Home

--- src/components/Main/Protected/Home.jsx
+++ src/components/Main/Protected/Home.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 export default function Home() {
+  // set document title
+  useEffect(() => {
+    document.title = "React Auth Template - Home";
+  }, []);
+
   return (
     <main>
       <h1>Home page!!</h1>
