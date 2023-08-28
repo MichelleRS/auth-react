8551dc7915d53f57d62d9d3009e5ec8ef9e32d80
Michelle Ross <miross629@gmail.com>
2023-08-28 09:53:42 -0700

    User can sign up, sent to /Home

--- src/components/Main/Public/SignUp.jsx
+++ src/components/Main/Public/SignUp.jsx
@@ -1,17 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../../context/AuthContext.jsx";
+import { client } from "../../../services/client.js";
 
 export default function SignUp() {
-  // TODO state
-
-  // handle form submit
-  const handleSubmit = () => {
-    console.log("I pressed the submit button to sign up!!");
-  };
+  // constants for use in function
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const register = (email, password) => client.auth.signUp({ email, password });
+  // TODO initialize state for error handling
 
   // set document title
   useEffect(() => {
     document.title = "React Auth Template - Sign Up";
   }, []);
+
+  // send users to home page
+  const { user } = useAuth();
+  if (user) {
+    return <Navigate to="/" />;
+  }
+
+  // handle form submit
+  const handleSubmit = async (e) => {
+    console.log("I pressed the submit button to sign up!!");
+    e.preventDefault();
+    // TODO error handling
+    try {
+      const { data, error } = await register(
+        emailRef.current.value,
+        passwordRef.current.value
+      );
+      if (!error && data) {
+        console.log("Sign up was a success!");
+      }
+    } catch (error) {
+      // TODO
+    }
+  };
 
   return (
     <>
@@ -24,6 +50,7 @@ export default function SignUp() {
           id="signUpEmail"
           name="SignUpEmail"
           autoComplete="email"
+          ref={emailRef}
           required
         />
         {/* password */}
@@ -33,6 +60,7 @@ export default function SignUp() {
           id="signUpPassword"
           name="signUpPassword"
           autoComplete="off"
+          ref={passwordRef}
           required
         />
         {/* submit */}
