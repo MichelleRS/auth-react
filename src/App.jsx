b1657cf3049c3a4cd092162f0ce55fe5db211b48
Michelle Ross <miross629@gmail.com>
2023-08-31 16:36:53 -0700

    Refactor App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./components/Main/Public/Layout.jsx";
-import SignIn from "./components/Main/Public/SignIn.jsx";
-import SignUp from "./components/Main/Public/SignUp.jsx";
-import Home from "./components/Main/Protected/Home.jsx";
-import ProtectedRoute from "./components/Main/Protected/ProtectedRoute.jsx";
-import AuthToolbar from "./components/Main/Public/AuthToolbar.jsx";
-import { useAuth } from "./context/AuthContext.jsx";
+import Main from "./components/Main/Main.jsx";
 
 function App() {
-  // get user
-  const { user } = useAuth();
-  if (user) {
-    return <Navigate to="/" />;
-  }
   return (
     <>
       <Header />
-      {/* TODO if not logged in, display AuthToolbar */}
-      {/* Main/ routes */}
-      {!user && <AuthToolbar />}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* public routes */}
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="sign-up" element={<SignUp />} />
-          {/* protected routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-          {/* TODO catch all */}
-        </Route>
-      </Routes>
+      <Main />
       <Footer />
     </>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,34 @@
+// import "./App.css";
+import "../../App.css";
+import { useAuth } from "../../context/AuthContext.jsx";
+import AuthToolbar from "./Public/AuthToolbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Public/Layout.jsx";
+import SignIn from "./Public/SignIn.jsx";
+import SignUp from "./Public/SignUp.jsx";
+import Home from "./Protected/Home.jsx";
+import ProtectedRoute from "./Protected/ProtectedRoute.jsx";
+
+export default function Main() {
+  // get user
+  const { user } = useAuth();
+
+  return (
+    <>
+      {/* if user, display sign in and sign out links */}
+      {!user && <AuthToolbar />}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="sign-in" element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
+          {/* protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          {/* TODO catch all */}
+        </Route>
+      </Routes>
+    </>
+  );
+}
