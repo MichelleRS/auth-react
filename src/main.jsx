3cfbd990106d2aab9aebbf6d41fb740039e99e59
Michelle Ross <miross629@gmail.com>
2023-09-07 13:20:33 -0700

    Refactor routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./context/AuthContext.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
