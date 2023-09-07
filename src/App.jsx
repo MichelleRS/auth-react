5a29d3cde63e78ab38267d928a43740bf677d1c9
Michelle Ross <miross629@gmail.com>
2023-09-07 13:44:52 -0700

    Set root path to Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import "./App.css";
-import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import Main from "./components/Main/Main.jsx";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Main/Public/Layout.jsx";
+import Account from "./components/Main/Public/Account.jsx";
+import ProtectedRoute from "./components/Main/Protected/ProtectedRoute.jsx";
+import Home from "./components/Main/Protected/Home.jsx";
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Routes>
+        {/* set root path to Layout component */}
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="account" element={<Account />} />
+          {/* protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          {/* TODO catch all */}
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/components/Main/Public/Account.jsx
@@ -0,0 +1,9 @@
+export default function Account() {
+  // TODO set document title
+  // TODO navigate user to home page
+  return (
+    <>
+      <h2>This is the account page!!</h2>
+    </>
+  );
+}
